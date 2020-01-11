3bb4613b63d60609a6db28e516b99ae06c08ce11
jjmin321 <jjmin321@naver.com>
2020-01-11 19:18:27 +0900

    [Update]

--- Server/server.js
+++ Server/server.js
@@ -14,27 +14,26 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 connection.query('SELECT * FROM users', function(err, rows, fields) {
-    if (!err)
+    if (!err){
       console.log('The solution is: ', rows);
+      console.log('Id is: ', rows[0].id);
+      console.log('Pw is: ', rows[0].pw);
+      console.log('Name is: ', rows[0].name);
+      console.log('Description is: ', rows[0].description);
+      console.log('user_created is: ', rows[0].user_created);
+    }
     else
       console.log('Error while performing Query.', err);
-  });
+    });
 //데이터베이스 연동
 
 
 //로그인 처리 및 토큰 생성
-var models = require("./models/index.js"); //Sequelize ORM
 var jwt = require("jsonwebtoken");
 var login = require('./config/jwt.js');
-// var router = express.Router();
-models.sequelize.sync().then( () => {
-  console.log(" DB 연결 성공");
-}).catch(err => {
-  console.log("연결 실패");
-  console.log(err);
-})
-app.get("/login", function(req,res,next){
-  res.send("hi");
+app.get("/login", function(req,res){
+  res.send("Hello JWT");
+  //토큰 내용
   var token = jwt.sign({
     id: "jjmin321"   // 토큰의 내용(payload)
   },
@@ -42,21 +41,8 @@ app.get("/login", function(req,res,next){
   {
     expiresIn: '5m'    // 유효 시간은 5분
   })
-  models.duo.find({
-    where: {
-       id: "jjmin321"
-    }
-  })
-  .then( duo => {
-    if(duo.pw === "qwerz123"){
-      res.cookie("user", token);
-      res.json({
-        token: token
-      })
-    }
-   })
- })
-module.exports = express.Router();
+  //토큰 내용
+});
 //로그인 처리 및 토큰 생성
 
 connection.end();
