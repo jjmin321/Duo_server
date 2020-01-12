b229b78c1936a27ccc49eca488f8b70c71208db5
jjmin321 <jjmin321@naver.com>
2020-01-12 16:57:09 +0900

    Update

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-var login = require('./routes/login.js')
-// 실험을 위한 코드
 //routes 안에 있어야 함. 
 var mysql = require('mysql');
 var jwt = require("jsonwebtoken");
 
+module.exports = function(app) {
 //데이터베이스
 var connection = mysql.createConnection({
     // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwerz123'
@@ -17,25 +13,29 @@ var connection = mysql.createConnection({
     database : 'duo'
 });
 connection.connect();
-var user_info = {
-  'id':'jjmin321',
-  'pw':'qwerz123',
-};
-var user_false = {
-  'id':'jjmin333',
-  'pw':'qwerz123',
-};
-console.log(user_info.id)
+var id = "jjmin321"
 //primary key 인 유저의 id가 디비에 존재하는지를 확인하는 코드
-connection.query("SELECT * FROM users where id = '"+user_info.id+"';", function(err, rows, fields) {
+connection.query("SELECT * FROM users where id = '"+id+"';", function(err, rows, fields) {
     if (!err){
-      if (rows[0].id === user_info.id && rows[0].pw === user_info.pw) //아이디랑 비번이 일치하면 jwt 토큰을 줌
-        console.log('The solution is: ', rows[0].id);
+        console.log('The solution is: ', rows);
+        // console.log('Id is: ', rows[0].id);
+        // console.log('Pw is: ', rows[0].pw);
+        // console.log('Name is: ', rows[0].name);
+        // console.log('Description is: ', rows[0].description);
+        // console.log('user_created is: ', rows[0].user_created);
     }
     else
     console.log('Error while performing Query.', err);
 connection.end();
 });
+
+//json
+var obj = {'key':'value'};
+console.log(obj)
+ console.log(
+             /* define stringify */
+             JSON.stringify(obj)
+ );
 
 //jwt
 app.get("/login", function(req,res){
@@ -50,5 +50,4 @@ app.get("/login", function(req,res){
     })
     //토큰 내용
 });
-// 실험을 위한 코드
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
