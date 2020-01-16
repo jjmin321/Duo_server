78de55cdad21ebdcea8a461ba98a1f689731d321
jjmin321 <jjmin321@naver.com>
2020-01-16 18:51:16 +0900

    JWT

--- Server/server.js
+++ Server/server.js
@@ -65,11 +65,40 @@ app.get('/api', function(req, res) {
   })
 });
 
-app.get('/api/protected', function(req, res) {
+app.post('/api/login', function(req, res){
+  //auth user
+  const user = {id : 3};
+  const token = jwt.sign({ user }, 'my_secret_key');
   res.json({
-    text: 'this is protected'
-  });
+    token:token
+  })
 });
+
+app.get('/api/protected', ensureToken, function(req, res) {
+  //토큰 받았는지 확인
+  jwt.verify(req.token, 'my_secret_key', function(err, data) {
+  if (err) {
+    res.sendStatus(403);
+  } else {
+    res.json({
+      text: 'this is protected',
+      data: data
+    }); 
+  }
+});
+});
+
+function ensureToken(req, res, next){
+  const bearerHeader = req.headers["authorization"];
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 //jwt 토큰 코드
 
 // 실험을 위한 코드
