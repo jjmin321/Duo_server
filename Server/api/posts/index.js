b7d02b608473184a731f5c30e95558da23374e53
jjmin321 <jjmin321@naver.com>
2020-02-18 19:22:37 +0900

    update File

--- Server/api/posts/index.js
+++ Server/api/posts/index.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const middlewareToken = require('../../middleware/auth');
 const addPost = require('./addPost.js');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    //파일 저장경로, 파일이름설정
+      destination: function (req, file, cb) {
+        cb(null, 'image/posts_image/');
+      },
+      filename: function (req, file, cb) {
+        cb(null, `${req.user}_${Date.now()}_${file.originalname}`);
+      },
+  });
+  
+  const upload = multer({ storage : storage })
 
 // /api/posts/addpost
 router.post('/addpost', middlewareToken, addPost);
